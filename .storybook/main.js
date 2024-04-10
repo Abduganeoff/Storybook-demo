195729d09b18e51018a1272f7416a6bd01a1b7c7
Muhammadrizo <abduganeoff@gmail.com>
2024-04-10 23:36:13 +0200

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,26 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
+  //    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  // "../src/stories/components/**/*.stories.@(js|jsx|ts|tsx|mdx)",
+
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-docs", // Ensures MDX support
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+};
+export default config;
